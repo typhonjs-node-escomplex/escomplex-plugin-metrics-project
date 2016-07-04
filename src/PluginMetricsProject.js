b1f879054b5acff14d4f30777e74be7bbbcb1981
typhonrt <support@typhonrt.org>
2016-07-04 16:46:04 -0700

    large overhaul / bug fixing of project metric gathering / calculation.

--- src/PluginMetricsProject.js
+++ src/PluginMetricsProject.js
@@ -1,9 +1,12 @@
-'use strict';
-
-import path from 'path';
+import MathUtil      from 'typhonjs-escomplex-commons/src/utils/MathUtil';
+import ModuleReport  from 'typhonjs-escomplex-commons/src/module/report/ModuleReport';
 
 /**
  * Provides default project metrics gathering and calculation.
+ *
+ * @see https://en.wikipedia.org/wiki/Adjacency_matrix
+ * @see https://en.wikipedia.org/wiki/Distance_matrix
+ * @see https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
  */
 export default class PluginMetricsProject
 {
@@ -33,18 +36,22 @@ export default class PluginMetricsProject
     */
    onProjectEnd(ev)
    {
-      this._createAdjacencyMatrix(ev.data.results);
+      const pathModule = ev.data.pathModule;
+      const projectResult = ev.data.results;
+
+      const adjacencyMatrix = this._calculateAdjacencyMatrix(pathModule, projectResult);
+
       if (!this.settings.noCoreSize)
       {
-         this._createVisibilityMatrix(ev.data.results);
-         this._setCoreSize(ev.data.results);
+         const visibilityMatrix = this._calculateVisibilityMatrix(projectResult, adjacencyMatrix);
+         this._calculateCoreSize(projectResult, visibilityMatrix);
       }
 
-      this._calculateAverages(ev.data.results);
+      this._calculateAverages(projectResult);
    }
 
    /**
-    * Stores settings and syntaxes.
+    * Stores settings.
     *
     * @param {object}   ev - escomplex plugin event data.
     */
@@ -60,362 +67,243 @@ export default class PluginMetricsProject
    // Project metrics calculation -----------------------------------------------------------------------------------
 
    /**
-    * adjacencyToDistMatrix
+    * Calculates an adjacency matrix for all modules based on ES Module and CommonJS dependencies also storing a
+    * compacted while returning the matrix for further calculation. Each row index corresponds to the same report index.
+    * Each row entry corresponds to a report index. These relationships dictate the dependencies between all
+    * report ModuleReports given the source paths.
     *
-    * @param {Array} matrix -
+    * @param {object}   pathModule - A module that conforms to the Node path API.
+    * @param {object}   projectResult - The ProjectResult being processed.
     *
-    * @returns {Array}
+    * @returns {Array<Array<number>>}
     * @private
     */
-   _adjacencyToDistMatrix(matrix)
+   _calculateAdjacencyMatrix(pathModule, projectResult)
    {
-      const distMatrix = [];
-      let i, j, value;
-      for (i = 0; i < matrix.length; i += 1)
-      {
-         distMatrix.push([]);
-         for (j = 0; j < matrix[i].length; j += 1)
-         {
-            // if i !== j and matrix value is undefined set distance to Infinity.
-            value = i === j ? 1 : matrix[i][j] || Infinity;
+      const reports = projectResult.reports;
+      const length = reports.length;
 
-            distMatrix[i][j] = value;
-         }
-      }
-      return distMatrix;
-   }
+      const adjacencyMatrix = MathUtil.create2DArray(length, 0);
 
-   /**
-    * calculateAverages
-    *
-    * @param {object}   results - The ESComplexProject results data.
-    *
-    * @private
-    */
-   _calculateAverages(results)
-   {
-      let divisor;
-
-      const sums = {
-         loc: 0,
-         cyclomatic: 0,
-         effort: 0,
-         params: 0,
-         maintainability: 0
-      };
-
-      if (results.reports.length === 0) { divisor = 1; }
-      else { divisor = results.reports.length; }
-
-      results.reports.forEach((report) =>
-      {
-         Object.keys(sums).forEach((key) => { sums[key] += report[key]; });
-      });
-
-      Object.keys(sums).forEach((key) =>
-      {
-         results[key] = sums[key] / divisor;
-      });
-   }
-
-   /**
-    * checkDependency
-    *
-    * @param {string}   from -
-    * @param {object}   dependency -
-    * @param {string}   to -
-    *
-    * @returns {*}
-    * @private
-    */
-   _checkDependency(from, dependency, to)
-   {
-      // Handle CJS dependencies
-      if (dependency.type === 'cjs')
-      {
-         if (this._isInternalCommonJSDependency(dependency)) { return this._isDependency(from, dependency, to); }
-
-         return false;
-      }
-
-      return this._isDependency(from, dependency, to);
-   }
-
-   /**
-    * Compares two numbers.
-    *
-    * @param {number}   lhs - Left-hand side.
-    * @param {number}   rhs - Right-hand side.
-    *
-    * @returns {number}
-    * @private
-    */
-   _compareNumbers(lhs, rhs)
-   {
-      return lhs < rhs ? -1 : lhs > rhs ? 1 : 0;
-   }
-
-   /**
-    * Compares two paths.
-    *
-    * @param {string}   lhs - Left-hand side.
-    * @param {string}   rhs - Right-hand side.
-    *
-    * @returns {number}
-    * @private
-    */
-   _comparePaths(lhs, rhs)
-   {
-      const lsplit = lhs.split(path.sep);
-      const rsplit = rhs.split(path.sep);
-
-      if (lsplit.length < rsplit.length || (lsplit.length === rsplit.length && lhs < rhs)) { return -1; }
-
-      if (lsplit.length > rsplit.length || (lsplit.length === rsplit.length && lhs > rhs)) { return 1; }
-
-      return 0;
-   }
-
-   /**
-    * createAdjacencyMatrix
-    *
-    * @param {object}   results - The ESComplexProject results data.
-    *
-    * @private
-    */
-   _createAdjacencyMatrix(results)
-   {
-      const adjacencyMatrix = new Array(results.reports.length);
       let density = 0;
 
-      results.reports.sort((lhs, rhs) => { return this._comparePaths(lhs.path, rhs.path); }).forEach((ignore, x) =>
+      for (let x = 0; x < length; x++)
       {
-         adjacencyMatrix[x] = new Array(results.reports.length);
-         results.reports.forEach((ignore, y) =>
+         for (let y = 0; y < length; y++)
          {
-            adjacencyMatrix[x][y] = this._getAdjacencyMatrixValue(results.reports, x, y);
-            if (adjacencyMatrix[x][y] === 1)
-            {
-               density += 1;
-            }
-         });
-      });
+            adjacencyMatrix[x][y] = x !== y && this._doesDependencyExist(pathModule, reports[x], reports[y]) ? 1 : 0;
 
-      results.adjacencyMatrix = adjacencyMatrix;
-      results.firstOrderDensity = this._percentifyDensity(density, adjacencyMatrix);
-   }
-
-
-   /**
-    * Implementation of Floyd Warshall algorithm for calculating visibility matrix in O(n^3) instead of O(n^4) with
-    * successive raising of powers.
-    *
-    * @param {object}   results - The ESComplexProject results data.
-    *
-    * @private
-    */
-   _createVisibilityMatrix(results)
-   {
-      let changeCost = 0, i, j, k, visibilityMatrix;
-
-      visibilityMatrix = this._adjacencyToDistMatrix(results.adjacencyMatrix);
-      const matrixLen = visibilityMatrix.length;
-
-      for (k = 0; k < matrixLen; k += 1)
-      {
-         for (i = 0; i < matrixLen; i += 1)
-         {
-            for (j = 0; j < matrixLen; j += 1)
-            {
-               if (visibilityMatrix[i][j] > visibilityMatrix[i][k] + visibilityMatrix[k][j])
-               {
-                  visibilityMatrix[i][j] = visibilityMatrix[i][k] + visibilityMatrix[k][j];
-               }
-            }
+            if (adjacencyMatrix[x][y] === 1) { density += 1; }
          }
       }
 
-      // convert back from a distance matrix to adjacency matrix, while also calculating change cost
-      visibilityMatrix = visibilityMatrix.map((row, rowIndex) =>
+      projectResult.adjacencyList = MathUtil.compactMatrix(adjacencyMatrix);
+
+      projectResult.firstOrderDensity = MathUtil.getPercent(density, length * length);
+
+      return adjacencyMatrix;
+   }
+
+   /**
+    * Calculates average ModuleReport metrics that are applicable to ProjectResult.
+    *
+    * @param {object}   projectResult - The ProjectResult being processed.
+    *
+    * @private
+    */
+   _calculateAverages(projectResult)
+   {
+      const divisor = projectResult.reports.length === 0 ? 1 : projectResult.reports.length;
+
+      const sums = ModuleReport.getProjectMetricSums();
+
+      // Defer to ModuleReport to sum all relevant module metrics applicable to ProjectResult.
+      projectResult.reports.forEach((report) => { report.sumMetrics(sums); });
+
+      Object.keys(sums).forEach((key) => { projectResult[key] = sums[key] / divisor; });
+   }
+
+   /**
+    * Calculates core size which is the percentage of modules / files that are both widely depended on and themselves
+    * depend on other modules. Lower is better.
+    *
+    * @param {object}               projectResult - The ProjectResult being processed.
+    * @param {Array<Array<number>>} visibilityMatrix - The calculated visibilityMatrix.
+    *
+    * @private
+    */
+   _calculateCoreSize(projectResult, visibilityMatrix)
+   {
+      if (projectResult.firstOrderDensity === 0)
       {
-         return row.map((value, columnIndex) =>
-         {
-            if (value < Infinity)
-            {
-               changeCost += 1;
-
-               if (columnIndex !== rowIndex)
-               {
-                  return 1;
-               }
-            }
-
-            return 0;
-         });
-      });
-
-      results.visibilityMatrix = visibilityMatrix;
-      results.changeCost = this._percentifyDensity(changeCost, visibilityMatrix);
-   }
-
-   /**
-    * doesDependencyExist
-    *
-    * @param {object}   from -
-    * @param {object}   to -
-    *
-    * @returns {*}
-    * @private
-    */
-   _doesDependencyExist(from, to)
-   {
-      return from.dependencies.reduce((result, dependency) =>
-      {
-         if (result === false) { return this._checkDependency(from.path, dependency, to.path); }
-
-         return true;
-      }, false);
-   }
-
-   /**
-    * getAdjacencyMatrixValue
-    *
-    * @param {object}   reports - The ESComplexModule reports data.
-    * @param {number}   x -
-    * @param {number}   y -
-    *
-    * @returns {number}
-    * @private
-    */
-   _getAdjacencyMatrixValue(reports, x, y)
-   {
-      if (x === y) { return 0; }
-
-      if (this._doesDependencyExist(reports[x], reports[y])) { return 1; }
-
-      return 0;
-   }
-
-   /**
-    * Gets the median value from the given array after sorting.
-    *
-    * @param {Array<number>}  values -
-    *
-    * @returns {number}
-    * @private
-    */
-   _getMedian(values)
-   {
-      values.sort(this._compareNumbers);
-
-      // Checks of values.length is odd.
-      if (values.length % 2) { return values[(values.length - 1) / 2]; }
-
-      return (values[(values.length - 2) / 2] + values[values.length / 2]) / 2;
-   }
-
-   /**
-    * isDependency
-    *
-    * @param {string}   from -
-    * @param {object}   dependency -
-    * @param {string}   to -
-    *
-    * @returns {boolean}
-    * @private
-    */
-   _isDependency(from, dependency, to)
-   {
-      let dependencyPath = dependency.path;
-
-      if (path.extname(dependencyPath) === '') { dependencyPath += path.extname(to); }
-
-      return path.resolve(path.dirname(from), dependencyPath) === to;
-   }
-
-   /**
-    * isInternalCommonJSDependency
-    *
-    * @param {object}   dependency -
-    *
-    * @returns {boolean}
-    * @private
-    */
-   _isInternalCommonJSDependency(dependency)
-   {
-      return dependency.path[0] === '.' &&
-       (dependency.path[1] === path.sep || (dependency.path[1] === '.' && dependency.path[2] === path.sep));
-   }
-
-   /**
-    * percentify
-    *
-    * @param {number}   value -
-    * @param {number}   limit -
-    *
-    * @returns {number}
-    * @private
-    */
-   _percentify(value, limit)
-   {
-      return limit === 0 ? 0 : (value / limit) * 100;
-   }
-
-   /**
-    * percentifyDensity
-    *
-    * @param {number}   density -
-    * @param {Array}    matrix -
-    *
-    * @returns {number}
-    * @private
-    */
-   _percentifyDensity(density, matrix)
-   {
-      return this._percentify(density, matrix.length * matrix.length);
-   }
-
-   /**
-    * Calculates core size.
-    *
-    * @param {object}   results - The ESComplexProject results data.
-    *
-    * @private
-    */
-   _setCoreSize(results)
-   {
-      if (results.firstOrderDensity === 0)
-      {
-         results.coreSize = 0;
+         projectResult.coreSize = 0;
          return;
       }
 
-      const fanIn = new Array(results.visibilityMatrix.length);
-      const fanOut = new Array(results.visibilityMatrix.length);
-      const boundaries = {};
+      const length = visibilityMatrix.length;
+
+      const fanIn = new Array(length);
+      const fanOut = new Array(length);
       let coreSize = 0;
 
-      results.visibilityMatrix.forEach((row, rowIndex) =>
+      for (let rowIndex = 0; rowIndex < length; rowIndex++)
       {
-         fanIn[rowIndex] = row.reduce((sum, value, valueIndex) =>
+         fanIn[rowIndex] = visibilityMatrix[rowIndex].reduce((sum, value, valueIndex) =>
          {
-            if (rowIndex === 0) { fanOut[valueIndex] = value; }
-            else { fanOut[valueIndex] += value; }
-
+            fanOut[valueIndex] = rowIndex === 0 ? value : fanOut[valueIndex] + value;
             return sum + value;
          }, 0);
-      });
+      }
 
       // Boundary values can also be chosen by looking for discontinuity in the
-      // distribution of values, but I've chosen the median to keep it simple.
-      boundaries.fanIn = this._getMedian(fanIn.slice());
-      boundaries.fanOut = this._getMedian(fanOut.slice());
-
-      results.visibilityMatrix.forEach((ignore, index) =>
+      // distribution of values, but to keep it simple the median is used.
+      const boundaries =
       {
-         if (fanIn[index] >= boundaries.fanIn && fanOut[index] >= boundaries.fanOut) { coreSize += 1; }
-      });
+         fanIn: MathUtil.getMedian(fanIn.slice()),
+         fanOut: MathUtil.getMedian(fanOut.slice())
+      };
 
-      results.coreSize = this._percentify(coreSize, results.visibilityMatrix.length);
+      for (let rowIndex = 0; rowIndex < length; rowIndex++)
+      {
+         if (fanIn[rowIndex] >= boundaries.fanIn && fanOut[rowIndex] >= boundaries.fanOut) { coreSize += 1; }
+      }
+
+      projectResult.coreSize = MathUtil.getPercent(coreSize, length);
+   }
+
+   /**
+    * Stores a compacted form of the visibility matrix. Each row index corresponds to the same report index.
+    * Each row entry corresponds to a report index. These relationships dictate the reverse visibility between all
+    * report ModuleReports which may indirectly impact the given module / file. The full matrix is returned for further
+    * calculation.
+    *
+    * Implementation of Floyd Warshall algorithm for calculating visibility matrix in O(n^3) instead of O(n^4) with
+    * successive raising of powers.
+    *
+    * @param {object}               projectResult - The ProjectResult being processed.
+    * @param {Array<Array<number>>} adjacencyMatrix - The calculated adjacencyMatrix.
+    *
+    * @return {Array<Array<number>>}
+    * @private
+    */
+   _calculateVisibilityMatrix(projectResult, adjacencyMatrix)
+   {
+      let changeCost = 0;
+
+      const length = adjacencyMatrix.length;
+      const visibilityMatrix = MathUtil.create2DArray(length, 0);
+
+      // Convert adjacency matrix to a distance matrix suitable for the Floyd Warshall algorithm.
+      // if i !== j and adjacency matrix value is 0 set distance to Infinity.
+      for (let x = 0; x < length; x++)
+      {
+         for (let y = 0; y < length; y++) { visibilityMatrix[x][y] = x === y ? 1 : adjacencyMatrix[x][y] || Infinity; }
+      }
+
+      // Floyd Warshall core algorithm
+      for (let k = 0; k < length; k++)
+      {
+         for (let x = 0; x < length; x++)
+         {
+            for (let y = 0; y < length; y++)
+            {
+               if (visibilityMatrix[x][y] > visibilityMatrix[x][k] + visibilityMatrix[k][y])
+               {
+                  visibilityMatrix[x][y] = visibilityMatrix[x][k] + visibilityMatrix[k][y];
+               }
+            }
+         }
+      }
+
+      // Convert back from a distance matrix to adjacency matrix while also calculating change cost.
+      for (let x = 0; x < length; x++)
+      {
+         for (let y = 0; y < length; y++)
+         {
+            if (visibilityMatrix[x][y] < Infinity)
+            {
+               changeCost++;
+
+               if (x !== y) { visibilityMatrix[x][y] = 1; }
+            }
+            else
+            {
+               visibilityMatrix[x][y] = 0;
+            }
+         }
+      }
+
+      projectResult.visibilityList = MathUtil.compactMatrix(visibilityMatrix);
+
+      projectResult.changeCost = MathUtil.getPercent(changeCost, length * length);
+
+      return visibilityMatrix;
+   }
+
+   /**
+    * Determines if there is at least one dependency that matches `toModuleReport.srcPath` from all the dependencies
+    * stored in `fromModuleReport`.
+    *
+    * @param {object}         pathModule - A module that conforms to the Node path API.
+    * @param {ModuleReport}   fromModuleReport - A ModuleReport to match to the srcPath of `toModuleReport`.
+    * @param {ModuleReport}   toModuleReport - A ModuleReport providing the `srcPath` to match.
+    *
+    * @returns {boolean}
+    * @private
+    */
+   _doesDependencyExist(pathModule, fromModuleReport, toModuleReport)
+   {
+      let matchedDependency = false;
+      let fromModuleReport_dirname = pathModule.dirname(fromModuleReport.srcPath);
+
+      for (let cntr = 0; cntr < fromModuleReport.dependencies.length; cntr++)
+      {
+         let depPath = fromModuleReport.dependencies[cntr].path;
+
+         // If there is no extension provided in the dependency then add the extension of the `to srcPath`.
+         if (pathModule.extname(depPath) === '') { depPath += pathModule.extname(toModuleReport.srcPath); }
+
+         // Best case match scenario when dependency matches toModuleReportPath.srcPath.
+         if (depPath === toModuleReport.srcPath)
+         {
+            matchedDependency = true;
+            break;
+         }
+
+         // Make sure that fromModuleReport dirname has the path separator prepended. This is necessary to make sure
+         // pathModule (Node.js path) treats `fromModuleReport_dirname` as the absolute root.
+         if (!fromModuleReport_dirname.startsWith(pathModule.sep))
+         {
+            fromModuleReport_dirname = `${pathModule.sep}${fromModuleReport_dirname}`;
+         }
+
+         if (pathModule.resolve(fromModuleReport_dirname, depPath) === toModuleReport.srcPath)
+         {
+            matchedDependency = true;
+            break;
+         }
+
+         let toModuleReport_modpath = toModuleReport.srcPath;
+
+         // Remove any local directory (`.`) leading character from `toModuleReport_modpath`.
+         if (toModuleReport_modpath.startsWith('.'))
+         {
+            toModuleReport_modpath = toModuleReport_modpath.replace(/^\./, '');
+         }
+
+         // Ensure `toModuleReport_modpath` starts with the path separator.
+         if (!toModuleReport_modpath.startsWith(pathModule.sep))
+         {
+            toModuleReport_modpath = `${pathModule.sep}${toModuleReport_modpath}`;
+         }
+
+         if (pathModule.resolve(fromModuleReport_dirname, depPath) === toModuleReport_modpath)
+         {
+            matchedDependency = true;
+            break;
+         }
+      }
+
+      return matchedDependency;
    }
 }
